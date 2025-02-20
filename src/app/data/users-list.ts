ace9a40701af3cc6645a33b76fe11916ee98c805
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-02-20 11:42:37 -0300

    Definindo a lista de usuários e as interfaces de usuarios, status e endereços

---
+++ src/app/data/users-list.ts
@@ -0,0 +1,119 @@
+import { IUser } from "../interfaces/user/user.interface";
+
+export const UsersList : IUser[] = [
+    {
+       nome: "Lucas Barbosa",
+       email: "lucas@gmail.com",
+       senha: "senha-segura-123",
+       idade: 22,
+       endereco: {
+        rua: "João Fechine",
+        numero: 214,
+        cidade: "Milagres",
+        estado: "CE",
+        pais: "Brasil"
+       },
+       telefone: "85999411992",
+       ativo: true,
+       funcao: "Desenvolvedor", 
+       dataCadastro: "2025-02-01T09:00:00.000Z",
+       status: {
+        online: true,
+        verificado: true,
+        assinaturaAtiva: true,
+        ultimoAcesso: "2025-02-19T09:00:00.000Z"
+       }
+    },
+    {
+        nome: "Luiz Fernando",
+        email: "luiz@gmail.com",
+        senha: "senha-segura-123",
+        idade: 22,
+        endereco: {
+         rua: "João Fechine",
+         numero: 214,
+         cidade: "Juazeiro do Norte",
+         estado: "CE",
+         pais: "Brasil"
+        },
+        telefone: "85999411992",
+        ativo: true,
+        funcao: "Desenvolvedor", 
+        dataCadastro: "2025-02-01T09:00:00.000Z",
+        status: {
+         online: true,
+         verificado: true,
+         assinaturaAtiva: true,
+         ultimoAcesso: "2025-02-19T09:00:00.000Z"
+        }
+    },
+    {
+        nome: "Leonardo Silva",
+        email: "leonardo@gmail.com",
+        senha: "senha-segura-123",
+        idade: 22,
+        endereco: {
+         rua: "João Fechine",
+         numero: 214,
+         cidade: "Juazeiro do Norte",
+         estado: "CE",
+         pais: "Brasil"
+        },
+        telefone: "85999411992",
+        ativo: true,
+        funcao: "Desenvolvedor", 
+        dataCadastro: "2025-02-01T09:00:00.000Z",
+        status: {
+         online: true,
+         verificado: true,
+         assinaturaAtiva: true,
+         ultimoAcesso: "2025-02-19T09:00:00.000Z"
+        }
+    },
+    {
+        nome: "Eduardo Aureliano",
+        email: "eduardo@gmail.com",
+        senha: "senha-segura-123",
+        idade: 22,
+        endereco: {
+         rua: "João Fechine",
+         numero: 214,
+         cidade: "Juazeiro do Norte",
+         estado: "CE",
+         pais: "Brasil"
+        },
+        telefone: "85999411992",
+        ativo: true,
+        funcao: "Desenvolvedor", 
+        dataCadastro: "2025-02-01T09:00:00.000Z",
+        status: {
+         online: true,
+         verificado: true,
+         assinaturaAtiva: true,
+         ultimoAcesso: "2025-02-19T09:00:00.000Z"
+        }
+    },
+    {
+        nome: "Vitor Sampaio",
+        email: "vitor@gmail.com",
+        senha: "senha-segura-123",
+        idade: 22,
+        endereco: {
+         rua: "João Fechine",
+         numero: 214,
+         cidade: "Juazeiro do Norte",
+         estado: "CE",
+         pais: "Brasil"
+        },
+        telefone: "85999411992",
+        ativo: true,
+        funcao: "Desenvolvedor", 
+        dataCadastro: "2025-02-01T09:00:00.000Z",
+        status: {
+         online: true,
+         verificado: true,
+         assinaturaAtiva: true,
+         ultimoAcesso: "2025-02-19T09:00:00.000Z"
+        }
+    }
+];
