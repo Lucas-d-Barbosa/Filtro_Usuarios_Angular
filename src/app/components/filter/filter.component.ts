3aaf1a455e8123f351d950545394687f7e958cde
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2025-03-05 19:22:49 -0300

    Armazenando os valores dos filtros

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {IFilterOptions} from "../../interfaces/IFilterOptions";
 @Component({
   selector: 'app-filter',
   standalone: false,
@@ -7,9 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
-  foods = [
-    {value: "value", viewValue: "Ativo"},
-    {value: "value", viewValue: "Inativo"},
+  // foods = [
+  //   {value: "value", viewValue: "Ativo"},
+  //   {value: "value", viewValue: "Inativo"},
 
+  // ]
+
+  filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined
+  };
+
+
+  statusList = [
+    { description: "Ativo", value: true },
+    { description: "Inativo", value: false }
   ]
 }
